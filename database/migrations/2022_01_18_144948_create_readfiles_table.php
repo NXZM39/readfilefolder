215a4bda48d2f5c8720d9736c7a070e264267427
NXZM39 <muhammadniezam29@gmail.com>
2022-01-19 00:09:53 +0800

    Migration for read file

--- database/migrations/2022_01_18_144948_create_readfiles_table.php
+++ database/migrations/2022_01_18_144948_create_readfiles_table.php
@@ -14,8 +14,32 @@ class CreateReadfilesTable extends Migration
     public function up()
     {
         Schema::create('readfiles', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('email_address');
+            $table->date('move_out_date')->format('d/m/Y');
+            $table->date('physical_move_out_date')->format('d/m/Y');
+            $table->time("move_out_time");
+            $table->unsignedInteger('recommendation_scale');
+            $table->string('reason_for_leaving');
+            $table->boolean('other_reason_for_leaving');
+            $table->string('understand_rating');
+            $table->string('thm_id');
+            $table->string('current_tenancy_id');
+            $table->unsignedInteger('indonesian_bank_account');
+            $table->string('indonesian_bank_account_number');
+            $table->string('indonesian_bank_name');
+            $table->boolean('bank_account_full_name');
+            $table->boolean('foreign_bank_account_number');
+            $table->boolean('foreign_bank_account_country');
+            $table->boolean('foreign_bank_account_currency');
+            $table->boolean('foreign_bank_account_iban');
+            $table->boolean('foreign_bank_account_swift');
+            $table->boolean('foreign_bank_account_other_details');
+            $table->string('submission_time_notice_1601976166415');
+            $table->string('submit_1561348107244');
+            $table->unsignedInteger('form_id');
             $table->timestamps();
+
         });
     }
 
